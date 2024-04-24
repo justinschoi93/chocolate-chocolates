a1947d560a39376ce3ac09f64751cc2bb177f76a
jOwl <jhome@js-MacBook-Pro-2.local>
2024-04-24 15:50:27 -0700

    debugging client/auth

--- server/config/auth.js
+++ server/config/auth.js
@@ -3,7 +3,7 @@ const { auth } = require('express-openid-connect');
 const config = {
   authRequired: false,
   auth0Logout: true,
-  baseURL: 'http://localhost:3000',
+  baseURL: 'http://localhost:5000',
   clientID: '4x0NQbBRFKuz8vbDyMkJIydipT2VHWzZ',
   issuerBaseURL: 'https://dev-rctjvozwd0neg2cg.us.auth0.com',
   secret: '8ed4f2edc1155e26cf620cf92a73baa338362479b9d2854201742c509096e484'

fcc4c769d4bf1a759fd16ef83f36ffd80f5575b1
jOwl <jhome@js-MacBook-Pro-2.local>
2024-04-24 15:49:28 -0700

    debugging client side authentication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Profile from './components/Profile/Profile';
+import LoginButton from './components/Authenticator/LoginButton';
+import LogoutButton from './components/Authenticator/LogoutButton';
+import { useAuth0 } from '@auth0/auth0-react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { isAuthenticated } = useAuth0();
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="app__container">
+      {isAuthenticated ? <LogoutButton /> : <LoginButton />}
+      <Profile />
+    </div>
   )
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Auth0Provider } from '@auth0/auth0-react'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <Auth0Provider
+    domain="dev-rctjvozwd0neg2cg.us.auth0.com"
+    clientId="4x0NQbBRFKuz8vbDyMkJIydipT2VHWzZ"
+    authorizationParams={{
+      redirect_uri: "http://localhost:5173"
+    }}
+  >
     <App />
-  </React.StrictMode>,
+  </Auth0Provider>
 )

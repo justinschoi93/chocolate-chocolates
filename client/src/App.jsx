6e04c34509af44e7a6610fbe73e49c95f8339d36
jOwl <jhome@js-MacBook-Pro-2.local>
2024-04-26 16:49:12 -0700

    typos

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,16 @@
-import Profile from './components/Profile/Profile';
-import LoginButton from './components/Authenticator/LoginButton';
-import LogoutButton from './components/Authenticator/LogoutButton';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 import Home from './components/Home/Home';
 import Header from './components/Header/Header';
-import { useAuth0 } from '@auth0/auth0-react';
 
 import './App.css'
 
 function App() {
-  const { isAuthenticated, error, isLoading } = useAuth0();
 
   return (
     <ErrorBoundary>
     <Header />
     <div className="app__container">
-      {
-        isAuthenticated ? 
-        <div className="isAuthenticated">
-          <div className="user"></div>
-          
-          <Home/>
-        </div>      
-        : 
-        <div className="isNotAuthenticated">
-          <Home/>
-        </div>
-      }
+      <Home/>
     </div>
     </ErrorBoundary>
   )
